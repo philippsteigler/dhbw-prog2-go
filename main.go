a8aaca5adb32b397442d2b426af16a5f28f79a3d
roman.wuertemberger@gmail.com <Goltenhof74>
2018-11-05 15:13:53 +0100

    Webserver der den Dateipfad auf einer Webseite anzeigt (http://localhost:0420)

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package ticketBackend
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":0420", nil))
+
+}
