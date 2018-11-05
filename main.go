908a5472ff8752302cbe4d29484e61177ec08fff
squanchy11 <philipp.steigler@mailbox.org>
2018-11-05 15:03:15 +0100

    Basic webserver w/ HTTP handler.

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
-package ticketBackend
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+var mux map[string]func(http.ResponseWriter, *http.Request)
+
+func main() {
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: &myHandler{},
+	}
+
+	mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	mux["/"] = hello
+
+	server.ListenAndServe()
+}
+
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h, ok := mux[r.URL.String()]; ok {
+		h(w, r)
+		return
+	}
+
+	io.WriteString(w, "Not found: "+r.URL.String())
+}
