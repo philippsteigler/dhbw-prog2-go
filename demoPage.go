5667cf77ad264aeda23b8b27bc90904b2e739383
squanchy11 <philipp.steigler@mailbox.org>
2018-11-05 18:18:38 +0100

    Extracted demo webpage for tests.

---
+++ demoPage.go
@@ -0,0 +1,23 @@
+package main
+
+// index page
+const indexPage = `
+<h1>Login</h1>
+<form method="post" action="/login">
+    <label for="name">User name</label>
+    <input type="text" id="name" name="name">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+    <button type="submit">Login</button>
+</form>
+`
+
+// internal page
+const internalPage = `
+<h1>Internal</h1>
+<hr>
+<small>User: %s</small>
+<form method="post" action="/logout">
+    <button type="submit">Logout</button>
+</form>
+`
