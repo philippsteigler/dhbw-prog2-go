f743e1cefa3333fff4134d911d88c1dab49dab2f
mennexi <wagnerjarno@gmail.com>
2018-11-15 21:05:05 +0100

    Funktion: Ticket anlegen weitere Funktionen zur verarbeitung hinzugefügt tests implementiert id generator implementiert

--- pageHandler/ticketPage.go
+++ pageHandler/ticketPage.go
@@ -10,13 +10,11 @@ import (
 //Speichert den Text aus den Textareas in mail, subject, text
 func SaveTicketHandler(response http.ResponseWriter, request *http.Request) {
 	if sessionHandler.IsUserLoggedIn(request) {
-
 		// Neues Ticket erzeugen
 		ticket.NewTicket(request.FormValue("ticketSubject"), request.FormValue("ticketMail"), request.FormValue("ticketText"))
 
 		//Zurück zu der Ticketseite
 		http.Redirect(response, request, "/ticket", http.StatusFound)
-
 	} else {
 		http.Redirect(response, request, "/", 302)
 	}
