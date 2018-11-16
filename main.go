1a073bc607128ea714b8db2a8e8131eeb0fb4d5f
squanchy11 <philipp.steigler@mailbox.org>
2018-11-16 09:27:54 +0100

    documentation

--- main.go
+++ main.go
@@ -47,26 +47,30 @@ func ticketPageHandler(response http.ResponseWriter, request *http.Request) {
 	}
 }
 
-// Golang webserver example:
+// A-3.1:
+// Die Web-Seite soll nur per HTTPS erreichbar sein.
+//
+// Die Startfunktion initialisiert die Anwendung und startet anschließend den Server.
+// Eingehende HTTP-Anfragen auf Webseiten werden hier geroutet.
 // https://github.com/jimmahoney/golang-webserver/blob/master/webserver.go
 func main() {
 	createDirIfNotExist("./assets/tickets")
 	port := 8000
-	portstring := strconv.Itoa(port)
+	portString := strconv.Itoa(port)
 	mux := http.NewServeMux()
 
-	// Webpages
+	// Webseiten routen
 	mux.Handle("/", http.HandlerFunc(indexPageHandler))
 	mux.Handle("/internal", http.HandlerFunc(internalPageHandler))
 	mux.Handle("/ticket", http.HandlerFunc(ticketPageHandler))
 
-	// Interactions
+	// Interaktionen verarbeiten (z.B. Buttons, ...)
 	mux.Handle("/login", http.HandlerFunc(sessionHandler.LoginHandler))
 	mux.Handle("/logout", http.HandlerFunc(sessionHandler.LogoutHandler))
 	mux.Handle("/saveTicket", http.HandlerFunc(pageHandler.SaveTicketHandler))
 
-	log.Print("Listening on port " + portstring + " ... ")
-	err := http.ListenAndServeTLS(":"+portstring, "./assets/certificates/server.crt", "./assets/certificates/server.key", mux)
+	log.Print("Listening on port " + portString + " ... ")
+	err := http.ListenAndServeTLS(":"+portString, "./assets/certificates/server.crt", "./assets/certificates/server.key", mux)
 	if err != nil {
 		log.Fatal("ListenAndServe error: ", err)
 	}
